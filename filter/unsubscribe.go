e8945b5ab953457eee1ee677e89895a5868db60b
Jia Wei <jwei@linkedin.com>
2016-07-21 16:01:50 +0800

    add strategy for unsubscriber filter in order to ignore proceess for some sms templates

--- filter/unsubscribe.go
+++ filter/unsubscribe.go
@@ -1,17 +1,25 @@
 package filter
 
 import (
+	"encoding/json"
+
 	"github.com/linkedin-inc/mane/logger"
 	t "github.com/linkedin-inc/mane/template"
 )
 
+type UnsubscribeStrategy struct {
+	Ignore bool `json:"ignore"`
+}
+
 type UnsubscribeFilter struct {
-	Type Type `bson:"type" json:"type"`
+	Type       Type            `bson:"type" json:"type"`
+	Strategies map[t.Name]bool `bson:"strategies" json:"strategies"`
 }
 
 func NewUnsubscribeFilter() *UnsubscribeFilter {
 	return &UnsubscribeFilter{
-		Type: FilterTypeUnsubscribe,
+		Type:       FilterTypeUnsubscribe,
+		Strategies: make(map[t.Name]bool),
 	}
 }
 
@@ -26,6 +34,10 @@ func RegisterUnsubscribeChecker(c UnsubscribeChecker) {
 }
 
 func (f *UnsubscribeFilter) Allow(phone string, template t.Name) bool {
+	ignored, existed := f.Strategies[template]
+	if existed && ignored {
+		return true
+	}
 	if unsubscribechecker.Exists(phone) {
 		logger.I("[sms] phone:%s template:%v prevented by UnsubscribeFilter", phone, template)
 		return false
@@ -38,10 +50,26 @@ func (f *UnsubscribeFilter) WhichType() Type {
 }
 
 func (f *UnsubscribeFilter) Apply(strategies []Strategy) {
-	//do nothing
+	if len(strategies) == 0 {
+		return
+	}
+	for _, strategy := range strategies {
+		resolved, err := f.Resolve(strategy.Expression)
+		if err != nil {
+			//FIXME discard when resolve failed?
+			logger.E("occur error when resolve strategy[%v] expression[%v]: %v\n", strategy.Type, strategy.Expression, err)
+			continue
+		}
+		f.Strategies[strategy.Template] = resolved.(UnsubscribeStrategy).Ignore
+	}
 }
 
 func (f *UnsubscribeFilter) Resolve(expression string) (interface{}, error) {
-	//do nothing
-	return nil, nil
+	//resolve expression to UnsubscribeStratege
+	var resolved UnsubscribeStrategy
+	err := json.Unmarshal([]byte(expression), &resolved)
+	if err != nil {
+		return nil, ErrResolveFailed
+	}
+	return resolved, nil
 }
